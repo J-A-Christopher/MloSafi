3dee102fe86c1b44e5eb1f32b92b426409b227d5
J-A-Christopher <christopherjesse45@gmail.com>
2024-04-27 19:17:14 +0300

    Implemented search feature

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
-import Food from "../models/Food";
+import Food, { IFood } from "../models/Food";
 import Category from "../models/Category";
 import SubCategory from "../models/Subcategory";
 
@@ -33,23 +33,42 @@ const getFoodCategory = async (
 };
 
 const getFoodSubCategory = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const categoryId = req.params.categoryId;
-      const categoryData = await SubCategory.findOne({ categoryId });
-      if (categoryData) {
-        return res.status(200).json({ categoryData });
-      } else {
-        return res.status(400).json({ message: "Subcategory not found" });
-      }
-    } catch (e) {
-      console.log(e);
-      next(new Error());
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const categoryData = await SubCategory.findOne({ categoryId });
+    if (categoryData) {
+      return res.status(200).json({ categoryData });
+    } else {
+      return res.status(400).json({ message: "Subcategory not found" });
     }
-  };
+  } catch (e) {
+    console.log(e);
+    next(new Error());
+  }
+};
 
+const search = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const query: string = req.query.q as string;
+    if (!query || query.trim() === "") {
+      return res
+        .status(400)
+        .json({ message: "Query parameter is missing or empty" });
+    }
+    const searchResults: IFood[] = await Food.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { description: { $regex: query, $options: "i" } },
+      ],
+    }).populate("category");
+    res.json({ results: searchResults });
+  } catch (error) {
+    next(new Error());
+  }
+};
 
-export { getFood, getFoodCategory, getFoodSubCategory };
+export { getFood, getFoodCategory, getFoodSubCategory, search };
--- src/routes/foodroutes.ts
+++ src/routes/foodroutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware";
-import { getFood, getFoodCategory, getFoodSubCategory } from "../controllers/foodController";
+import { getFood, getFoodCategory, getFoodSubCategory, search } from "../controllers/foodController";
 
 const router = express.Router();
 router.get("/all-foods", authenticate, getFood);
 router.get("/food-category/:foodId", authenticate, getFoodCategory);
 router.get("/food-subcategory/:categoryId", authenticate, getFoodSubCategory);
+router.get("/search", authenticate,search );
 
 export default router;
