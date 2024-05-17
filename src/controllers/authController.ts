23f38261b313f71c1816aa748b09f337030e371e
J-A-Christopher <christopherjesse45@gmail.com>
2024-05-17 21:30:44 +0300

    Revamped the entire auth object to only allow the token for both mobile and web

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,12 +1,15 @@
-import { Request, Response , NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import User from "../models/User";
 import {
   generateToken,
-  generateTokenForMobile,
   clearToken,
 } from "../utils/auth";
 
-const registerUser = async (req: Request, res: Response, next: NextFunction) => {
+const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { fname, lname, username, email, password } = req.body;
   const userExists = await User.findOne({ email });
   try {
@@ -21,56 +24,51 @@ const registerUser = async (req: Request, res: Response, next: NextFunction) => 
         email: user.email,
       });
     }
-    
   } catch (e) {
-   return res
-    .status(400)
-    .json({ message: "An error occurred while creating the user" });
+    return res
+      .status(400)
+      .json({ message: "An error occurred while creating the user" });
   }
 };
 
-const authenticateUser = async (req: Request, res: Response, next:NextFunction) => {
+const authenticateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   try {
     if (!user) {
       return res.status(401).json({ message: "User not found" });
     }
-    if (!user!.comparePassword(password)) {
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
       return res.status(401).json({ message: "Incorrect password" });
     }
-    if (user && user.comparePassword(password)) {
-      if (
-        req.headers["user-agent"] &&
-        req.headers["user-agent"].includes("Mobile")
-      ) {
-        const token = generateTokenForMobile(user._id, user.username, user.email);
-        return res.status(200).json({ token });
-      } else {
-        generateToken(res, user._id, user.username, user.email);
-      }
-      return res.status(201).json({
-        id: user._id,
-        username: user.username,
-        email: user.email,
-      });
-    }
-    
-  } catch (e) {
-  next(new Error())
-  }
- 
-};
 
-const logOutUser = async (req: Request, res: Response, next:NextFunction) => {
-  try {
-    clearToken(res);
-    return res.status(200).json({ message: "User logged out" });
-    
+    if (user && user.comparePassword(password)) {
+      const isMobile = req.useragent && req.useragent.isMobile;
+      console.log(!isMobile);
+      console.log("User-Agent:", req.headers["user-agent"]);
+      const token = generateToken(user._id, user.username, user.email);
+
+      return res.status(201).json({ token });
+    }
   } catch (e) {
     next(new Error());
   }
- 
+};
+
+const logOutUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    clearToken(res);
+    return res.status(200).json({ message: "User logged out" });
+  } catch (e) {
+    next(new Error());
+  }
 };
 
 export { registerUser, authenticateUser, logOutUser };
--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -12,30 +12,36 @@ interface AuthenticatedRequest extends Request {
 const authenticate = asyncHandler(
   async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-      let token = req.cookies.jwt;
+      // console.log(req);
+      let token = req.headers.token;
+      console.log("token------------", token);
 
       if (!token) {
         res.status(401);
         throw new AuthenticationError("Token not found");
       }
       const jwtSecret = process.env.JWT_SECRET || "defaultSecret";
+      console.log(jwtSecret);
 
-      const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
-      if (!decoded || !decoded.userId) {
-        res.status(401);
-        throw new AuthenticationError("UserId not found");
+      const decoded = jwt.verify(token as string, jwtSecret) as JwtPayload;
+      console.log("decode-------", decoded);
+      if (typeof decoded !== "string") {
+        if (!decoded || !decoded.userId) {
+          res.status(401);
+          throw new AuthenticationError("UserId not found");
+        }
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+          res.status(401);
+          throw new AuthenticationError("User not found");
+        }
+        req.user = user;
       }
-      const user = await User.findById(decoded.userId);
-      if (!user) {
-        res.status(401);
-        throw new AuthenticationError("User not found");
-      }
-      req.user = user;
       next();
     } catch (error) {
+      console.log(error)
       throw new AuthenticationError("Invalid token");
     }
   }
 );
-
 export { authenticate };
